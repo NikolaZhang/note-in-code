36e57b17a34478d32708ca8c06e086521ae4ec59
nikola <nikolazhang@163.com>
2021-02-07 23:30:09 +0800

    remove custom config and add gitignore

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,6 @@ import { HelloWorld } from './component/hello/HelloWorld';
 export function activate(context: vscode.ExtensionContext) {
 
     console.info('Congratulations, your extension "note-in-code" is now active!');
-    console.info(vscode.workspace.workspaceFolders);
-    console.info(vscode.workspace.workspaceFolders[0]);
     new HelloWorld().apply(context);
     new TemplateCommand().apply(context);
 
