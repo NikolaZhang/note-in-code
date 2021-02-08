de1378878bdfb435d02565d2e6c29c6745916909
nikola <nikolazhang@163.com>
2021-02-09 07:05:24 +0800

    tongbu

--- src/component/notebook/command/TemplateCommand.ts
+++ src/component/notebook/command/TemplateCommand.ts
@@ -10,10 +10,10 @@ import { BaseNoteCommand } from "./BaseNoteCommand";
  *
  * @author: zhangxu
  */
+const TEMPLATE_PATH = "nic.template.path";
 export class TemplateCommand extends BaseNoteCommand implements IMakeComponent {
-    category: string = "template";
-    nicConfig: WorkspaceConfiguration;
-    templates: string = `
+  category: string = "template";
+  templates: string = `
         {
             "cc": ["[nic-begin title=$1 ["
                 
@@ -22,85 +22,98 @@ export class TemplateCommand extends BaseNoteCommand implements IMakeComponent {
         }
     `;
 
-    constructor() {
-        super();
-        this.init();
+  constructor() {
+    super();
+    this.init();
+  }
+
+  protected getCategory(): string {
+    return this.category;
+  }
+
+  apply(context: vscode.ExtensionContext): void {
+    let add = vscode.commands.registerCommand(this.getCmdName("add"), () => {
+      this.add();
+    });
+    let update = vscode.commands.registerCommand(
+      this.getCmdName("update"),
+      () => {
+        this.update();
+      }
+    );
+    let remove = vscode.commands.registerCommand(
+      this.getCmdName("remove"),
+      () => {
+        this.remove();
+      }
+    );
+    let query = vscode.commands.registerCommand(
+      this.getCmdName("query"),
+      () => {
+        this.query();
+      }
+    );
+
+    context.subscriptions.push(add, update, remove, query);
+  }
+
+  check(): boolean {
+    // more check
+    return super.check(TEMPLATE_PATH);
+  }
+
+  /**
+   * 初始化模板配置需要的路径和文件
+   */
+  init() {
+    if (!this.check()) {
+      console.warn(
+        "cannot find nic config, we create it under current workspace, " +
+          "named nic_template.json with directory .nic!"
+      );
+      let fileRoot = vscode.workspace.workspaceFolders[0].uri;
+      let targetDir: Uri = Uri.joinPath(fileRoot, ".nic");
+      // 在当前目录下创建 .nic目录, 并添加nic.json文件
+      vscode.workspace.fs.createDirectory(targetDir);
+      let filePath: Uri = Uri.joinPath(targetDir, "nic_template.json");
+
+      vscode.workspace.fs.writeFile(
+        filePath,
+        new TextEncoder().encode(this.templates)
+      );
+
+      // 更新配置路径
+      vscode.workspace.getConfiguration().update(TEMPLATE_PATH, filePath.path);
+      console.info("now we put templates under " + filePath.path);
     }
+  }
 
-    protected getCategory(): string {
-        return this.category;
-    }
-
-    apply(context: vscode.ExtensionContext): void {
-        let add = vscode.commands.registerCommand(
-            this.getCmdName("add"),
-            () => {
-                this.add();
-            }
-        );
-        let update = vscode.commands.registerCommand(
-            this.getCmdName("update"),
-            () => {
-                this.update();
-            }
-        );
-        let remove = vscode.commands.registerCommand(
-            this.getCmdName("remove"),
-            () => {
-                this.remove();
-            }
-        );
-        let query = vscode.commands.registerCommand(
-            this.getCmdName("query"),
-            () => {
-                this.query();
-            }
-        );
-
-        context.subscriptions.push(add, update, remove, query);
-    }
-
-    check(): boolean {
-        // more check
-        return super.check("nic.template.path");
-    }
-
-    /**
-     * 初始化模板配置需要的路径和文件
-     */
-    init() {
-        if (!this.check()) {
-            console.warn("cannot find nic config, we create it!");
-            let fileRoot = vscode.workspace.workspaceFolders[0].uri;
-            let targetDir: Uri = Uri.joinPath(fileRoot, ".nic", "templates");
-            // 在当前目录下创建 .nic目录, 并添加nic.json文件
-            vscode.workspace.fs.createDirectory(targetDir);
-            let filePath: Uri = Uri.joinPath(targetDir, "nic.json");
-
-            vscode.workspace.fs.writeFile(
-                filePath,
-                new TextEncoder().encode(this.templates)
-            );
-
-            // 更新配置路径
-            vscode.workspace
-                .getConfiguration()
-                .update("nic.template.path", filePath.path);
-            console.warn("now we put templates under " + filePath.path);
-        }
-    }
-
-    /**
-     * 添加模板
-     */
-    // todo zx finish those method
-    add() {
-        vscode.window.showInputBox({
-            "placeHolder": "请添加模板",
-            "validateInput": (input) => { return null;}
-        });
-    }
-    update() {}
-    remove() {}
-    query() {}
+  /**
+   * 添加模板
+   */
+  // todo zx finish those method
+  add() {
+    vscode.window
+      .showInputBox({
+        placeHolder: "请添加模板",
+        validateInput: (val) => {
+          try {
+            return JSON.parse(val);
+          } catch (error) {
+            console.log("模板格式错误");
+            return null;
+          }
+        },
+      })
+      .then((val) => {
+        // 将模板追加到文件中
+        let path: string = vscode.workspace
+          .getConfiguration()
+          .get<string>(TEMPLATE_PATH);
+        vscode.workspace.fs.writeFile(Uri.parse(path), new TextEncoder().encode(val));
+      });
+  }
+  update() {}
+  remove() {}
+  query() {}
 }
